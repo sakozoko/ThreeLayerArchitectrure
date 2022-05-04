f50da0af42ab0878d43e297f29ea0654a285f796
sakozoko <olesis.ko@icloud.com>
2022-05-04 21:33:21 +0300

    Incorrect method TryParseLoginAndPassword in LoginCommand.cs fixed

--- ConsolesShop/Command/LoginCommand.cs
+++ ConsolesShop/Command/LoginCommand.cs
@@ -10,6 +10,7 @@ public class LoginCommand : BasicCommand
     private readonly IUser[] _users;
     private string _name;
     private string _password;
+    private const int CountOfArgs = 4;
 
     public LoginCommand(IUser[] users) : base(Names)
     {
@@ -25,9 +26,14 @@ public class LoginCommand : BasicCommand
     {
         if (!TryParseLoginAndPassword(args)) return;
             var user = TryFindUserByName();
-            if (user is RegisteredUser us)
+            switch (user)
             {
-                Console.WriteLine(us.Login(_password) ? "Login successful" : "Password incorrect");
+                case null:
+                    Console.WriteLine("Name incorrect");
+                    break;
+                case RegisteredUser us:
+                    Console.WriteLine(us.Login(_password) ? "Login successful" : "Password incorrect");
+                    break;
             }
     }
 
@@ -39,17 +45,25 @@ public class LoginCommand : BasicCommand
     {
         if (args is null)
             return false;
+        var nameIsParsed = false;
+        var passwordIsParsed=false;
         try
         {
-            for (int i = 0; i < args.Length; i++)
+            for (int i = 0; i < CountOfArgs; i++)
             {
                 if (args[i] == "-n")
+                {
                     _name = args[i + 1];
-                if (args[i] == "-p")
-                    _password = args[i + 1];
-            }
+                    nameIsParsed = true;
+                }
 
-            return true;
+                if (args[i] == "-p")
+                {
+                    _password = args[i + 1];
+                    passwordIsParsed = true;
+                }
+            }
+            return nameIsParsed && passwordIsParsed;
         }
         catch (IndexOutOfRangeException)
         {
