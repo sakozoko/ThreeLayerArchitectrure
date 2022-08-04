9ef32ea52f2df11adad0612cc0880f95a5834449
sakozoko <olesis.ko@icloud.com>
2022-08-04 15:37:35 +0300

    AutoMapperHandler is single instance

--- BLL/Util/ServiceContainerModule.cs
+++ BLL/Util/ServiceContainerModule.cs
@@ -11,7 +11,7 @@ public class ServiceContainerModule : Module
     protected override void Load(ContainerBuilder builder)
     {
         builder.RegisterModule(new UnitOfWorkModule());
-        builder.RegisterType<AutoMapperHandler>().As<IMapperHandler>();
+        builder.RegisterType<AutoMapperHandler>().As<IMapperHandler>().SingleInstance();
         builder.RegisterType<DebugLogger>().As<ILogger>();
         builder.RegisterType<CustomTokenHandler>().As<ITokenHandler>();
         builder.RegisterType<ServiceContainer>().As<IServiceContainer>();
