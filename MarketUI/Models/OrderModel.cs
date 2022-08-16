84cd64cacb29b0ac704cc13ee7ff9b8b964f1330
sakozoko <olesis.ko@icloud.com>
2022-08-16 11:24:25 +0300

    refactoring order model

--- MarketUI/Models/OrderModel.cs
+++ MarketUI/Models/OrderModel.cs
@@ -6,7 +6,7 @@ namespace MarketUI.Models;
 public class OrderModel:BaseModel
 {
     public UserModel Owner { get; set; }
-    public virtual IList<ProductModel> Products { get; set; }
+    public  IList<ProductModel> Products { get; set; } = new List<ProductModel>();
     public string Description { get; set; }
     public string OrderStatus { get; set; }
     public decimal Total => Products.Sum(p=>p.Cost);
