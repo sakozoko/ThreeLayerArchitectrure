efb004c4055f092f92443180cdafe32de8e9bd03
sakozoko <olesis.ko@icloud.com>
2022-08-18 20:03:06 +0300

    used ConsoleTable in OrderHistoryViewCommand

--- MarketUI/Command/OrderHistoryViewCommand.cs
+++ MarketUI/Command/OrderHistoryViewCommand.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using BLL;
 using MarketUI.Models;
+using MarketUI.Util;
 using MarketUI.Util.Interface;
 
 namespace MarketUI.Command;
@@ -21,37 +21,34 @@ public class OrderHistoryViewCommand : BaseCommand
 
     public override string Execute(string[] args)
     {
-        var stringBuilder = new StringBuilder();
-        stringBuilder.Append("ID \t Description \t Status");
-        IEnumerable<OrderModel> res;
-        if (TryParseId(args))
-        {
-            var taskUser = _serviceContainer.UserService.GetById(ConsoleUserInterface.AuthenticationData?.Token, _id);
-            res = Mapper.Map<IEnumerable<OrderModel>>(_serviceContainer.OrderService.GetUserOrders(
-                ConsoleUserInterface.AuthenticationData?.Token,
-                taskUser.Result).Result);
-        }
-        else
-        {
-            res = Mapper.Map<IEnumerable<OrderModel>>(_serviceContainer.OrderService
-                .GetUserOrders(ConsoleUserInterface.AuthenticationData?.Token).Result);
-        }
+        var consoleTable = new ConsoleTable();
+        consoleTable.AddColumn("#", "Description", "Status" );
+        
+        var targetUser = TryParseId(args)
+            ? _serviceContainer.UserService.GetById(ConsoleUserInterface.AuthenticationData?.Token, _id).Result
+            : null;
+        
+        var orderModels = Mapper.Map<IEnumerable<OrderModel>>(_serviceContainer.OrderService
+                .GetUserOrders(ConsoleUserInterface.AuthenticationData?.Token,targetUser).Result);
+        
 
-        foreach (var order in res)
+        foreach (var order in orderModels)
         {
-            stringBuilder.Append($"\n {order.Id} \t {order.Description} \t {order.OrderStatus}");
-            stringBuilder.Append("\n\tProducts:");
+            consoleTable.AddRow(order.Id, order.Description, order.OrderStatus);
+            var consoleTable2 = new ConsoleTable(consoleTable.GetCalculatedPadding());
+            if(order.Products.Count==0)
+                continue;
+            
+            consoleTable2.AddColumn( "Name", "Count", "Sum");
 
             foreach (var orderProductGroup in order.Products.GroupBy(x => x.Id))
-                stringBuilder.Append(
-                    $"\n {orderProductGroup.First().Name} \t  {orderProductGroup.Count()}\t  {orderProductGroup.Sum(x => x.Cost)}");
-
-            stringBuilder.Append("\n-------------------------------------------------------");
-            stringBuilder.Append("\n\tTotal: " + order.Total);
+                consoleTable2.AddRow(orderProductGroup.First().Name, orderProductGroup.Count(),
+                    orderProductGroup.Sum(x => x.Cost));
+            consoleTable.AddRowWithoutColumn(new string(' ', 15)+"Products:\n" + consoleTable2 + "\n"+new string(' ', 15)+"Total: " + order.Total);
         }
 
 
-        return stringBuilder.ToString();
+        return consoleTable.ToString();
     }
 
     private bool TryParseId(string[] args)
