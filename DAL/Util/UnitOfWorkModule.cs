da658093038f0464ce28aaddbeeec60fff71d28c
sakozoko <olesis.ko@icloud.com>
2022-08-31 21:31:18 +0300

    remove useless MoqDbContext.cs

--- DAL/Util/UnitOfWorkModule.cs
+++ DAL/Util/UnitOfWorkModule.cs
@@ -7,9 +7,7 @@ public class UnitOfWorkModule : Module
 {
     protected override void Load(ContainerBuilder builder)
     {
-        //builder.RegisterType<DbContext>().As<IDbContext>().SingleInstance();
-        //using moq instead collection
-        builder.RegisterInstance(new MoqDbContext().MoqDb);
+        builder.RegisterType<DbContext>().As<IDbContext>().SingleInstance();
         builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
     }
 }
